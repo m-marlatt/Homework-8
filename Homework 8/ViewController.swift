c58373cf176fbaaa4a97d4d2018307779c91c89f
Max Marlatt <m.marlatt@setonhill.edu>
2020-04-08 19:13:52 -0400

    Homework 8

--- Homework 8/ViewController.swift
+++ Homework 8/ViewController.swift
@@ -9,10 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    @IBAction func tapAct(_ sender: Any) {
+        outputLabel.text = "Coke"
+    }
+    
+    
+    @IBAction func swipeAct(_ sender: Any) {
+        outputLabel.text = "Pepsi"
+    }
+    
+    @IBAction func longAct(_ sender: Any) {
+        outputLabel.text = "RC"
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
