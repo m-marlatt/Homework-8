1f7519d9628ddeee5383106526ad58d372126a67
Max Marlatt <m.marlatt@setonhill.edu>
2020-04-08 11:56:12 -0400

    Initial Commit

---
+++ Homework 8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework 8
+//
+//  Created by Maxwell Marlatt on 4/8/20.
+//  Copyright © 2020 Maxwell Marlatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
